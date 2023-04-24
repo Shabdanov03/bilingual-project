1f429555d8be7fd2ec2bb27c03ca1fd826ee663f
ShabdanovIlim <sabdanovilim@gmail.com>
2023-04-24 17:21:40 +0600

    feature/added validations

--- src/main/java/com/example/bilingualb8/dto/requests/auth/SignUpRequest.java
+++ src/main/java/com/example/bilingualb8/dto/requests/auth/SignUpRequest.java
@@ -1,19 +1,20 @@
 package com.example.bilingualb8.dto.requests.auth;
 
+import com.example.bilingualb8.validations.NameValid;
+import com.example.bilingualb8.validations.PasswordValid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record SignUpRequest(
-        @Pattern(regexp = "^[а-яА-ЯёЁa-zA-Z]+(([',. -][а-яА-ЯёЁa-zA-Z ])?[а-яА-ЯёЁa-zA-Z]*)*$", message = "The surname must contain only letters.")
+        @NameValid
         @NotBlank(message = "The first name must not be empty.")
         @NotNull(message = "The first name must not be empty.")
         String firstName,
 
-        @Pattern(regexp = "^[а-яА-ЯёЁa-zA-Z]+(([',. -][а-яА-ЯёЁa-zA-Z ])?[а-яА-ЯёЁa-zA-Z]*)*$", message = "The surname must contain only letters.")
+        @NameValid
         @NotBlank(message = "The surname must not be empty.")
         @NotNull(message = "The surname must not be empty.")
         String lastName,
@@ -25,7 +26,7 @@ public record SignUpRequest(
 
         @NotBlank(message = "The password must not be empty.")
         @NotNull(message = "The password must not be empty.")
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "The password must contain at least one letter in , one number and be at least 8 characters long.")
+        @PasswordValid
         String password
 ) {
 }
