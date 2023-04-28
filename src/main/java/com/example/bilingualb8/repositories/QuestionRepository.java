d8e5631cace1a0cb4f879d5faca39dd90f75e90a
alibekaltynbekuulu2002 <alibekprogramming@gmail.com>
2023-04-28 19:58:44 +0600

    commit update

--- src/main/java/com/example/bilingualb8/repositories/QuestionRepository.java
+++ src/main/java/com/example/bilingualb8/repositories/QuestionRepository.java
@@ -1,27 +1,4 @@
 package com.example.bilingualb8.repositories;
 
-<<<<<<< HEAD
-import com.example.bilingualb8.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question,Long> {
-=======
-import com.example.bilingualb8.dto.responses.question.RespondNWordsQuestionResponse;
-import com.example.bilingualb8.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    @Query("select new com.example.bilingualb8.dto.responses.question.RespondNWordsQuestionResponse" +
-            "(q.id,q.title,q.statement,q.duration,q.questionType,q.minWords,t.id) from Question q join q.test t where q.questionType = 'RESPOND_N_WORDS'")
-    List<RespondNWordsQuestionResponse> getAllRespondNWords();
-    @Query("select new com.example.bilingualb8.dto.responses.question.RespondNWordsQuestionResponse" +
-            "(q.id,q.title,q.statement,q.duration,q.questionType,q.minWords,t.id) from Question q join q.test t where q.questionType = 'RESPOND_N_WORDS' and q.id = ?1")
-    Optional<RespondNWordsQuestionResponse> findByIdRespondNWords(Long id);
->>>>>>> 347b759a835a654e3275416dc4d5558b652a66d8
+public interface QuestionRepository {
 }
