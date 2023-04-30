e08f4c1dff215f09ed0ed569d6035f7a6ea90849
ShabdanovIlim <sabdanovilim@gmail.com>
2023-04-30 17:02:51 +0600

    features/HighlightTheAnswer-crud-methods

--- src/main/java/com/example/bilingualb8/controllers/question/HighlightTheAnswerQuestionController.java
+++ src/main/java/com/example/bilingualb8/controllers/question/HighlightTheAnswerQuestionController.java
@@ -1,4 +1,41 @@
 package com.example.bilingualb8.controllers.question;
 
+import com.example.bilingualb8.dto.requests.questions.highlight_the_answer.HighlightTheAnswerQuestionRequest;
+import com.example.bilingualb8.dto.requests.questions.highlight_the_answer.HighlightTheAnswerQuestionUpdateRequest;
+import com.example.bilingualb8.dto.responses.SimpleResponse;
+import com.example.bilingualb8.dto.responses.questions.highlight_the_answer.HighlightTheAnswerQuestionResponse;
+import com.example.bilingualb8.services.questions.HighlightTheAnswerQuestionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/questions/highlight-the-answer")
+@RequiredArgsConstructor
 public class HighlightTheAnswerQuestionController {
+    private final HighlightTheAnswerQuestionService questionService;
+    @PostMapping("/save")
+    public SimpleResponse saveHighlightTheAnswerQuestion(@RequestBody @Valid HighlightTheAnswerQuestionRequest questionRequest) {
+        return questionService.saveHighlightTheAnswerQuestion(questionRequest);
+    }
+    @GetMapping("/all")
+    public List<HighlightTheAnswerQuestionResponse> getAllHighlightTheAnswerQuestion(){
+        return questionService.getAllHighlightTheAnswerQuestion();
+    }
+    @GetMapping("/{id}")
+    public HighlightTheAnswerQuestionResponse getHighlightTheAnswerQuestionById(@PathVariable Long id){
+        return questionService.getHighlightTheAnswerQuestionById(id);
+    }
+    @DeleteMapping("/{id}/delete")
+    public SimpleResponse deleteHighlightTheAnswerQuestionById(@PathVariable Long id){
+        return questionService.deleteHighlightTheAnswerQuestionById(id);
+    }
+    @PutMapping("/{id}/update")
+    public SimpleResponse updateHighlightTheAnswerQuestionById(@PathVariable Long id, @RequestBody
+    @Valid HighlightTheAnswerQuestionUpdateRequest updateRequest){
+        return questionService.updateHighlightTheAnswerQuestionById(id,updateRequest);
+    }
+
 }
--- src/main/java/com/example/bilingualb8/dto/requests/questions/highlight_the_answer/HighlightTheAnswerQuestionRequest.java
+++ src/main/java/com/example/bilingualb8/dto/requests/questions/highlight_the_answer/HighlightTheAnswerQuestionRequest.java
@@ -1,4 +1,36 @@
 package com.example.bilingualb8.dto.requests.questions.highlight_the_answer;
 
+import com.example.bilingualb8.enums.QuestionType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class HighlightTheAnswerQuestionRequest {
+    @NotNull(message = "The title must not be empty !")
+    private String title;
+    @Builder.Default
+    @NotNull(message = "The question type must not be empty.")
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType = QuestionType.HIGHLIGHT_THE_ANSWER;
+    @NotNull(message = "The statement must not be empty.")
+    private String statement;
+    @NotNull(message = "The passage must not be empty.")
+    private String passage;
+    @NotNull(message = "The correct answer must not be empty.")
+    private String correctAnswer;
+    @NotNull(message = "The duration must not be empty.")
+    @Positive(message = "Duration can not be negative")
+    private Integer duration;
+    @NotNull(message = "The question order must not be empty.")
+    @Positive(message = "Order can not be negative")
+    private Integer questionOrder;
+    @NotNull(message = "The test id must not be empty.")
+    @Positive(message = "Test id can not be negative")
+    private Long testId;
+
 }
