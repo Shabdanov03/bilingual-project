3730d8666687a951d51c01e686da25b6d93f5f5a
alibekaltynbekuulu2002 <alibekprogramming@gmail.com>
2023-05-10 19:26:20 +0600

    fixed is_active

--- src/main/java/com/example/bilingualb8/dto/requests/test/TestRequest.java
+++ src/main/java/com/example/bilingualb8/dto/requests/test/TestRequest.java
@@ -5,14 +5,11 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record TestRequest (
-        @NotBlank(message = "The title must not be empty.")
         @NotNull(message = "The title must not be empty.")
         @Length(min = 5, max = 70)
         String title,
-        @NotBlank(message = "The description must not be empty.")
         @NotNull(message = "The description must not be empty.")
         String shortDescription,
-        @NotBlank(message = "The isActive must not be empty.")
         @NotNull(message = "The isActive must not be empty.")
         Boolean isActive
 ){
