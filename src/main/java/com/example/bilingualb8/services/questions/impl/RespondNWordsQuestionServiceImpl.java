81268ed556cc4944c30a104e41e5f8a38039bcba
saltanat <saltanatnematillakyzy@gmail.com>
2023-05-23 01:07:05 +0600

    Added logger to methods

--- src/main/java/com/example/bilingualb8/services/questions/impl/RespondNWordsQuestionServiceImpl.java
+++ src/main/java/com/example/bilingualb8/services/questions/impl/RespondNWordsQuestionServiceImpl.java
@@ -11,10 +11,7 @@ import com.example.bilingualb8.repositories.QuestionRepository;
 import com.example.bilingualb8.repositories.TestRepository;
 import com.example.bilingualb8.services.questions.RespondNWordsQuestionService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
